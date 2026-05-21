02245d288c7056c24c5c6b45e667c47e5d7a00d1
Monu <monugandhi5911@gmail.com>
2026-05-22 02:13:08 +0530

    chore: wipe non-essential files and functions to fix build

--- check.cjs
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const data = JSON.parse(fs.readFileSync('lint_report.json', 'utf8')); data.forEach(f => { if (f.messages.length > 0) { console.log(f.filePath.split('\\\\').pop()); f.messages.forEach(m => console.log('  ' + m.line + ':' + m.column + ' - ' + m.ruleId)); } });
