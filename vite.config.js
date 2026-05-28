078409bf58c3bef8dc5d14e97fca84c6be8a0b31
Monu <monugandhi5911@gmail.com>
2026-05-28 13:48:48 +0530

    fix: inline service worker registration for pwabuilder

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'inline',
       workbox: {
         skipWaiting: true,
         clientsClaim: true,
